593c480a7661682d1fd968b854bc95e729b82ab6
201st-Luka <luka.eichten@gmail.com>
2024-04-02 10:35:24 +0200

    implemented datagen for dragon fire

--- src/main/java/be/noah/ritual_magic/datagen/ModBlockStateProvider.java
+++ src/main/java/be/noah/ritual_magic/datagen/ModBlockStateProvider.java
@@ -2,10 +2,12 @@ package be.noah.ritual_magic.datagen;
 
 import be.noah.ritual_magic.RitualMagic;
 import be.noah.ritual_magic.block.ModBlocks;
+import net.minecraft.data.DataProvider;
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.models.ModelProvider;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
-import net.minecraftforge.client.model.generators.BlockStateProvider;
-import net.minecraftforge.client.model.generators.ModelFile;
+import net.minecraftforge.client.model.generators.*;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.registries.RegistryObject;
 
@@ -24,14 +26,115 @@ public class ModBlockStateProvider extends BlockStateProvider {
 
         simpleBlockWithItem(ModBlocks.ANCIENT_ANVIL.get(),
                 new ModelFile.UncheckedModelFile(modLoc("block/ancient_anvil")));
-        simpleBlockWithItem(ModBlocks.DRAGON_FIRE.get(),
-                new ModelFile.UncheckedModelFile(modLoc("block/dragon_fire")));
-
+//        simpleBlockWithItem(ModBlocks.DRAGON_FIRE.get(),
+//                new ModelFile.UncheckedModelFile(modLoc("block/dragon_fire_block")));
+        fireBlock(ModBlocks.DRAGON_FIRE);
     }
+
     private void blockWithItem(RegistryObject<Block> blockRegistryObject) {
         simpleBlockWithItem(blockRegistryObject.get(), cubeAll(blockRegistryObject.get()));
     }
-    private void fireblock(RegistryObject<Block> blockRegistryObject){
 
+    private void fireBlock(RegistryObject<Block> blockRegistryObject){
+        String name = blockRegistryObject.getId().getPath();
+
+        ModelFile test = models().singleTexture(
+                "block/" + name + "_side0",
+                new ResourceLocation("block/template_fire_side"),
+                "fire",
+                new ResourceLocation(RitualMagic.MODID, "block/" + name + "_0")
+        );
+
+        ModelFile floor0 = models().singleTexture(
+                "block/" + name + "_floor0",
+                new ResourceLocation("block/template_fire_floor"),
+                "fire",
+                new ResourceLocation(RitualMagic.MODID, "block/" + name + "_0")
+        ), floor1 = models().singleTexture(
+                "block/" + name + "_floor1",
+                new ResourceLocation("block/template_fire_floor"),
+                "fire",
+                new ResourceLocation(RitualMagic.MODID, "block/" + name + "_1")
+        ), side0 = models().singleTexture(
+                "block/" + name + "_side0",
+                new ResourceLocation("block/template_fire_side"),
+                "fire",
+                new ResourceLocation(RitualMagic.MODID, "block/" + name + "_0")
+        ), side1 = models().singleTexture(
+                "block/" + name + "_side1",
+                new ResourceLocation("block/template_fire_side"),
+                "fire",
+                new ResourceLocation(RitualMagic.MODID, "block/" + name + "_1")
+        ), side_alt0 = models().singleTexture(
+                "block/" + name + "_side_alt0",
+                new ResourceLocation("block/template_fire_side_alt"),
+                "fire",
+                new ResourceLocation(RitualMagic.MODID, "block/" + name + "_0")
+        ), side_alt1 = models().singleTexture(
+                "block/" + name + "_side_alt1",
+                new ResourceLocation("block/template_fire_side_alt"),
+                "fire",
+                new ResourceLocation(RitualMagic.MODID, "block/" + name + "_1")
+        );
+
+        getMultipartBuilder(blockRegistryObject.get())
+                .part()
+                    .modelFile(floor0)
+                    .nextModel()
+                    .modelFile(floor1)
+                    .addModel()
+                    .end()
+                .part()
+                    .modelFile(side0)
+                    .nextModel()
+                    .modelFile(side1)
+                    .nextModel()
+                    .modelFile(side_alt0)
+                    .nextModel()
+                    .modelFile(side_alt1)
+                    .addModel()
+                    .end()
+                .part()
+                    .modelFile(side0)
+                    .rotationY(90)
+                    .nextModel()
+                    .modelFile(side1)
+                    .rotationY(90)
+                    .nextModel()
+                    .modelFile(side_alt0)
+                    .rotationY(90)
+                    .nextModel()
+                    .modelFile(side_alt1)
+                    .rotationY(90)
+                    .addModel()
+                    .end()
+                .part()
+                    .modelFile(side0)
+                    .rotationY(180)
+                    .nextModel()
+                    .modelFile(side1)
+                    .rotationY(180)
+                    .nextModel()
+                    .modelFile(side_alt0)
+                    .rotationY(180)
+                    .nextModel()
+                    .modelFile(side_alt1)
+                    .rotationY(180)
+                    .addModel()
+                    .end()
+                .part()
+                    .modelFile(side0)
+                    .rotationY(270)
+                    .nextModel()
+                    .modelFile(side1)
+                    .rotationY(270)
+                    .nextModel()
+                    .modelFile(side_alt0)
+                    .rotationY(270)
+                    .nextModel()
+                    .modelFile(side_alt1)
+                    .rotationY(270)
+                    .addModel()
+                    .end();
     }
 }
