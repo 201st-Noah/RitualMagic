c6c44adb0fd00735df61f3e0fc7e3348bd2e840f
201ts-Luka <81482503+201st-Luka@users.noreply.github.com>
2025-06-06 16:28:18 +0200

    implemented infusion recipe datagen

--- src/main/java/be/noah/ritual_magic/datagen/ModRecipeProvider.java
+++ src/main/java/be/noah/ritual_magic/datagen/ModRecipeProvider.java
@@ -2,15 +2,18 @@ package be.noah.ritual_magic.datagen;
 
 import be.noah.ritual_magic.RitualMagic;
 import be.noah.ritual_magic.block.ModBlocks;
+import be.noah.ritual_magic.datagen.recipe.InfusionRecipeBuilder;
 import be.noah.ritual_magic.item.ModItems;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.*;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.AbstractCookingRecipe;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.level.ItemLike;
 import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.function.Consumer;
@@ -22,84 +25,237 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
         super(pOutput);
     }
 
+    protected static void oreSmelting(
+            @NotNull Consumer<FinishedRecipe> pFinishedRecipeConsumer,
+            List<ItemLike> pIngredients,
+            @NotNull RecipeCategory pCategory,
+            @NotNull ItemLike pResult,
+            float pExperience,
+            int pCookingTIme,
+            @NotNull String pGroup
+    ) {
+        oreCooking(
+                pFinishedRecipeConsumer,
+                RecipeSerializer.SMELTING_RECIPE,
+                pIngredients,
+                pCategory,
+                pResult,
+                pExperience,
+                pCookingTIme,
+                pGroup,
+                "_from_smelting"
+        );
+    }
+
+    protected static void oreBlasting(
+            @NotNull Consumer<FinishedRecipe> pFinishedRecipeConsumer,
+            @NotNull List<ItemLike> pIngredients,
+            @NotNull RecipeCategory pCategory,
+            @NotNull ItemLike pResult,
+            float pExperience,
+            int pCookingTime,
+            @NotNull String pGroup
+    ) {
+        oreCooking(
+                pFinishedRecipeConsumer,
+                RecipeSerializer.BLASTING_RECIPE,
+                pIngredients,
+                pCategory,
+                pResult,
+                pExperience,
+                pCookingTime,
+                pGroup,
+                "_from_blasting"
+        );
+    }
+
+    protected static void oreCooking(
+            @NotNull Consumer<FinishedRecipe> pFinishedRecipeConsumer,
+            @NotNull RecipeSerializer<? extends AbstractCookingRecipe> pCookingSerializer,
+            @NotNull List<ItemLike> pIngredients,
+            @NotNull RecipeCategory pCategory,
+            @NotNull ItemLike pResult,
+            float pExperience,
+            int pCookingTime,
+            @NotNull String pGroup,
+            String pRecipeName
+    ) {
+        for (ItemLike itemlike : pIngredients) {
+            SimpleCookingRecipeBuilder.generic(
+                            Ingredient.of(itemlike),
+                            pCategory,
+                            pResult,
+                            pExperience,
+                            pCookingTime,
+                            pCookingSerializer
+                    )
+                    .group(pGroup)
+                    .unlockedBy(getHasName(itemlike), has(itemlike))
+                    .save(
+                            pFinishedRecipeConsumer,
+                            RitualMagic.MODID + ":" + getItemName(pResult) + pRecipeName + "_" + getItemName(itemlike)
+                    );
+        }
+
+    }
+
     @Override
-    protected void buildRecipes(Consumer<FinishedRecipe> pWriter) {
-        oreSmelting(pWriter, List.of(ModBlocks.DWARVEN_DEBRIS.get()), RecipeCategory.MISC ,ModItems.DWARVEN_SCRAP.get(), 0.25f, 200,"dwarven_scrap");
-        oreBlasting(pWriter, List.of(ModBlocks.DWARVEN_DEBRIS.get()), RecipeCategory.MISC ,ModItems.DWARVEN_SCRAP.get(), 0.25f, 100,"dwarven_scrap");
+    protected void buildRecipes(@NotNull Consumer<FinishedRecipe> pWriter) {
+        oreSmelting(pWriter,
+                List.of(ModBlocks.DWARVEN_DEBRIS.get()),
+                RecipeCategory.MISC,
+                ModItems.DWARVEN_SCRAP.get(),
+                0.25f,
+                200,
+                "dwarven_scrap");
+        oreBlasting(pWriter,
+                List.of(ModBlocks.DWARVEN_DEBRIS.get()),
+                RecipeCategory.MISC,
+                ModItems.DWARVEN_SCRAP.get(),
+                0.25f,
+                100,
+                "dwarven_scrap");
 
-        oreSmelting(pWriter, List.of(ModBlocks.ATLANTIAN_DEBRIS.get()), RecipeCategory.MISC ,ModItems.ATLANTIAN_SCRAP.get(), 0.25f, 200,"atlantian_scrap");
-        oreBlasting(pWriter, List.of(ModBlocks.ATLANTIAN_DEBRIS.get()), RecipeCategory.MISC ,ModItems.ATLANTIAN_SCRAP.get(), 0.25f, 100,"atlantian_scrap");
+        oreSmelting(pWriter,
+                List.of(ModBlocks.ATLANTIAN_DEBRIS.get()),
+                RecipeCategory.MISC, ModItems.ATLANTIAN_SCRAP.get(),
+                0.25f,
+                200,
+                "atlantian_scrap");
+        oreBlasting(pWriter,
+                List.of(ModBlocks.ATLANTIAN_DEBRIS.get()),
+                RecipeCategory.MISC, ModItems.ATLANTIAN_SCRAP.get(),
+                0.25f,
+                100,
+                "atlantian_scrap");
 
-        oreSmelting(pWriter, List.of(ModBlocks.PETRIFIED_DRAGON_SCALE.get()), RecipeCategory.MISC ,ModItems.DRAGON_SCALE.get(), 0.25f, 200,"dragon_scale");
-        oreBlasting(pWriter, List.of(ModBlocks.PETRIFIED_DRAGON_SCALE.get()), RecipeCategory.MISC ,ModItems.DRAGON_SCALE.get(), 0.25f, 100,"dragon_scale");
+        oreSmelting(pWriter,
+                List.of(ModBlocks.PETRIFIED_DRAGON_SCALE.get()),
+                RecipeCategory.MISC,
+                ModItems.DRAGON_SCALE.get(),
+                0.25f,
+                200,
+                "dragon_scale");
+        oreBlasting(pWriter,
+                List.of(ModBlocks.PETRIFIED_DRAGON_SCALE.get()),
+                RecipeCategory.MISC,
+                ModItems.DRAGON_SCALE.get(),
+                0.25f,
+                100,
+                "dragon_scale");
 
         ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.DWARVEN_TEMPLATE.get())
-                .requires(Items.SILENCE_ARMOR_TRIM_SMITHING_TEMPLATE,1)
-                .requires(Items.WARD_ARMOR_TRIM_SMITHING_TEMPLATE,1)
-                .requires(Items.SHAPER_ARMOR_TRIM_SMITHING_TEMPLATE,1)
-                .requires(Items.HOST_ARMOR_TRIM_SMITHING_TEMPLATE,1)
+                .requires(Items.SILENCE_ARMOR_TRIM_SMITHING_TEMPLATE, 1)
+                .requires(Items.WARD_ARMOR_TRIM_SMITHING_TEMPLATE, 1)
+                .requires(Items.SHAPER_ARMOR_TRIM_SMITHING_TEMPLATE, 1)
+                .requires(Items.HOST_ARMOR_TRIM_SMITHING_TEMPLATE, 1)
                 .unlockedBy(getHasName(ModItems.DWARVEN_STEEL_INGOT.get()), has(ModItems.DWARVEN_STEEL_INGOT.get()))
                 .save(pWriter);
         ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.DWARVEN_STEEL_INGOT.get())
-                .requires(ModItems.DWARVEN_SCRAP.get(),4)
-                .requires(ModItems.WARDEN_CORE.get(),1)
-                .requires(Items.IRON_INGOT,4)
+                .requires(ModItems.DWARVEN_SCRAP.get(), 4)
+                .requires(ModItems.WARDEN_CORE.get(), 1)
+                .requires(Items.IRON_INGOT, 4)
                 .unlockedBy(getHasName(ModItems.DWARVEN_SCRAP.get()), has(ModItems.DWARVEN_SCRAP.get()))
-                .save(pWriter);
+                .group("dwarven_steel_ingot")
+                .save(pWriter, new ResourceLocation(RitualMagic.MODID, "dwarven_steel_ingot_from_scrap"));
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.DWARVEN_STEEL_INGOT.get(), 9)
+                .requires(ModBlocks.DWARVEN_STEEL_BLOCK.get())
+                .unlockedBy(getHasName(ModItems.DWARVEN_STEEL_INGOT.get()), has(ModItems.DWARVEN_STEEL_INGOT.get()))
+                .group("dwarven_steel_ingot")
+                .save(pWriter, new ResourceLocation(RitualMagic.MODID, "dwarven_steel_ingot_from_block"));
         ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.DWARVEN_STEEL_BLOCK.get())
                 .pattern("AAA")
                 .pattern("AAA")
                 .pattern("AAA")
-                .define('A',ModItems.DWARVEN_STEEL_INGOT.get())
+                .define('A', ModItems.DWARVEN_STEEL_INGOT.get())
                 .unlockedBy(getHasName(ModItems.DWARVEN_STEEL_INGOT.get()), has(ModItems.DWARVEN_STEEL_INGOT.get()))
                 .save(pWriter);
-        //Doppelte Rezepte gehen nicht wtf
-        /*ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.DWARVEN_STEEL_INGOT.get(),9)
-                .requires(ModBlocks.DWARVEN_STEEL_BLOCK.get())
-                .unlockedBy(getHasName(ModItems.DWARVEN_STEEL_INGOT.get()), has(ModItems.DWARVEN_STEEL_INGOT.get()))
-                .save(pWriter);*/
-        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, Items.HEART_OF_THE_SEA,2)
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, Items.HEART_OF_THE_SEA, 2)
                 .pattern("ITI")
                 .pattern("NHN")
                 .pattern("ITI")
-                .define('T',Items.SCUTE)
-                .define('N',Items.NAUTILUS_SHELL)
-                .define('I',Items.BLUE_ICE)
-                .define('H',Items.HEART_OF_THE_SEA)
+                .define('T', Items.SCUTE)
+                .define('N', Items.NAUTILUS_SHELL)
+                .define('I', Items.BLUE_ICE)
+                .define('H', Items.HEART_OF_THE_SEA)
                 .unlockedBy(getHasName(Items.HEART_OF_THE_SEA), has(Items.HEART_OF_THE_SEA))
                 .save(pWriter);
-        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.BUILDERSTAFF.get(),1 )
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.BUILDERSTAFF.get(), 1)
                 .pattern("  I")
                 .pattern(" S ")
                 .pattern("S  ")
-                .define('S',Items.STICK)
-                .define('I',ModItems.DWARVEN_STEEL_INGOT.get())
+                .define('S', Items.STICK)
+                .define('I', ModItems.DWARVEN_STEEL_INGOT.get())
                 .unlockedBy(getHasName(ModItems.DWARVEN_STEEL_INGOT.get()), has(ModItems.DWARVEN_STEEL_INGOT.get()))
                 .save(pWriter);
-        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.TORCH.get(),1 )
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.TORCH.get(), 1)
                 .pattern(" C ")
                 .pattern(" S ")
                 .pattern(" S ")
-                .define('S',Items.STICK)
-                .define('C',Items.COAL_BLOCK)
+                .define('S', Items.STICK)
+                .define('C', Items.COAL_BLOCK)
                 .unlockedBy(getHasName(Items.COAL_BLOCK), has(Items.COAL_BLOCK))
                 .save(pWriter);
-    }
-    protected static void oreSmelting(Consumer<FinishedRecipe> pFinishedRecipeConsumer, List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult, float pExperience, int pCookingTIme, String pGroup) {
-        oreCooking(pFinishedRecipeConsumer, RecipeSerializer.SMELTING_RECIPE, pIngredients, pCategory, pResult, pExperience, pCookingTIme, pGroup, "_from_smelting");
-    }
 
-    protected static void oreBlasting(Consumer<FinishedRecipe> pFinishedRecipeConsumer, List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult, float pExperience, int pCookingTime, String pGroup) {
-        oreCooking(pFinishedRecipeConsumer, RecipeSerializer.BLASTING_RECIPE, pIngredients, pCategory, pResult, pExperience, pCookingTime, pGroup, "_from_blasting");
-    }
-
-    protected static void oreCooking(Consumer<FinishedRecipe> pFinishedRecipeConsumer, RecipeSerializer<? extends AbstractCookingRecipe> pCookingSerializer, List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult, float pExperience, int pCookingTime, String pGroup, String pRecipeName) {
-        for(ItemLike itemlike : pIngredients) {
-            SimpleCookingRecipeBuilder.generic(Ingredient.of(itemlike), pCategory, pResult,
-                    pExperience, pCookingTime, pCookingSerializer)
-                    .group(pGroup).unlockedBy(getHasName(itemlike), has(itemlike))
-                    .save(pFinishedRecipeConsumer, RitualMagic.MODID + ":" +getItemName(pResult) + pRecipeName + "_" + getItemName(itemlike));
-        }
-
+        new InfusionRecipeBuilder().input(ModItems.ICE_SWORD_HILT.get())
+                .pedestalItems(
+                        ModItems.SHARP_ICE_SHARD.get(),
+                        ModItems.SHARP_ICE_SHARD.get(),
+                        ModItems.SHARP_ICE_SHARD.get(),
+                        ModItems.SHARP_ICE_SHARD.get()
+                )
+                .result(ModItems.ICE_SWORD.get())
+                .manaType("ATLANTIAN")
+                .minBlockTier("BASIC")
+                .preserveMainItem(false)
+                .save(pWriter);
+        new InfusionRecipeBuilder().input(ModItems.ICE_SWORD.get())
+                .pedestalItems(ModItems.ICE_SHARD.get())
+                .result(ModItems.ICE_SWORD.get())
+                .minLevel(0)
+                .maxLevel(99)
+                .levelGain(1)
+                .manaCost(0)
+                .manaType("ATLANTIAN")
+                .minBlockTier("BASIC")
+                .preserveMainItem(true)
+                .save(pWriter);
+        new InfusionRecipeBuilder().input(ModItems.ICE_SWORD.get())
+                .pedestalItems(ModItems.ATLANTIAN_STEEL_INGOT.get())
+                .result(ModItems.ICE_SWORD.get())
+                .minLevel(0)
+                .maxLevel(90)
+                .levelGain(10)
+                .manaCost(0)
+                .manaType("ATLANTIAN")
+                .minBlockTier("BASIC")
+                .preserveMainItem(true)
+                .save(pWriter);
+        new InfusionRecipeBuilder().input(ModItems.ICE_SHARD.get())
+                .pedestalItems(
+                        ModItems.ICE_SHARD.get(),
+                        ModItems.ICE_SHARD.get(),
+                        ModItems.ICE_SHARD.get(),
+                        ModItems.ICE_SHARD.get(),
+                        ModItems.ICE_SHARD.get(),
+                        ModItems.ICE_SHARD.get(),
+                        ModItems.ICE_SHARD.get(),
+                        ModItems.ICE_SHARD.get()
+                )
+                .result(ModItems.SHARP_ICE_SHARD.get())
+                .manaType("ATLANTIAN")
+                .minBlockTier("BASIC")
+                .save(pWriter);
+        new InfusionRecipeBuilder().input(ModItems.ATLANTIAN_STEEL_INGOT.get())
+                .pedestalItems(
+                        ModItems.ATLANTIAN_STEEL_INGOT.get(),
+                        ModItems.ATLANTIAN_STEEL_INGOT.get(),
+                        ModItems.ATLANTIAN_STEEL_INGOT.get(),
+                        ModItems.SHARP_ICE_SHARD.get()
+                )
+                .result(ModItems.ICE_SWORD_HILT.get())
+                .manaType("ATLANTIAN")
+                .minBlockTier("BASIC")
+                .save(pWriter);
     }
 }
---
+++ src/main/java/be/noah/ritual_magic/datagen/recipe/InfusionRecipeBuilder.java
@@ -0,0 +1,129 @@
+package be.noah.ritual_magic.datagen.recipe;
+
+import be.noah.ritual_magic.RitualMagic;
+import be.noah.ritual_magic.recipe.ModRecipes;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.crafting.Ingredient;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+public class InfusionRecipeBuilder {
+    private Ingredient input;
+    private List<Ingredient> pedestalItems;
+    private Ingredient result;
+    private int minLevel = 0, maxLevel = 0, levelGain = 0, manaCost = 0;
+    private String manaType = null, minBlockTier = null;
+    private boolean preserveMainItem = false;
+
+    public InfusionRecipeBuilder input(Item input) {
+        this.input = Ingredient.of(input);
+        return this;
+    }
+
+    public InfusionRecipeBuilder pedestalItems(Item... items) {
+        this.pedestalItems = Arrays.stream(items).map(Ingredient::of).collect(Collectors.toList());
+        return this;
+    }
+
+    public InfusionRecipeBuilder result(Item result) {
+        this.result = Ingredient.of(result);
+        return this;
+    }
+
+    public InfusionRecipeBuilder minLevel(int v) {
+        this.minLevel = v;
+        return this;
+    }
+
+    public InfusionRecipeBuilder maxLevel(int v) {
+        this.maxLevel = v;
+        return this;
+    }
+
+    public InfusionRecipeBuilder levelGain(int v) {
+        this.levelGain = v;
+        return this;
+    }
+
+    public InfusionRecipeBuilder manaCost(int v) {
+        this.manaCost = v;
+        return this;
+    }
+
+    public InfusionRecipeBuilder manaType(String v) {
+        this.manaType = v;
+        return this;
+    }
+
+    public InfusionRecipeBuilder minBlockTier(String v) {
+        this.minBlockTier = v;
+        return this;
+    }
+
+    public InfusionRecipeBuilder preserveMainItem(boolean v) {
+        this.preserveMainItem = v;
+        return this;
+    }
+
+    public void save(Consumer<FinishedRecipe> consumer) {
+        String path = "infusion/" + result.getItems()[0].getItem();
+        if (levelGain > 0)
+            path += "_leveling" + levelGain;
+        save(consumer, new ResourceLocation(RitualMagic.MODID, path));
+    }
+
+    public void save(Consumer<FinishedRecipe> consumer, ResourceLocation id) {
+        consumer.accept(new FinishedRecipe() {
+            @Override
+            public void serializeRecipeData(@NotNull JsonObject json) {
+                json.addProperty("type", "ritual_magic:infusion");
+                json.add("input", input.toJson());
+                json.add("pedestal_items", pedestalItems.stream()
+                        .map(Ingredient::toJson)
+                        .collect(JsonArray::new, JsonArray::add, JsonArray::addAll));
+                json.add("result", result.toJson());
+                json.addProperty("min_level", minLevel);
+                json.addProperty("max_level", maxLevel);
+                json.addProperty("level_gain", levelGain);
+                json.addProperty("mana_cost", manaCost);
+                if (manaType == null)
+                    throw new IllegalStateException("manaType must be set.");
+                json.addProperty("mana_type", manaType);
+                if (minBlockTier == null)
+                    throw new IllegalStateException("minBlockTier must be set.");
+                json.addProperty("min_block_tier", minBlockTier);
+                json.addProperty("preserve_main_item", preserveMainItem);
+            }
+
+            @Override
+            @NotNull
+            public ResourceLocation getId() {
+                return id;
+            }
+
+            @Override
+            @NotNull
+            public net.minecraft.world.item.crafting.RecipeSerializer<?> getType() {
+                return ModRecipes.INFUSION_SERIALIZER.get();
+            }
+
+            @Override
+            public JsonObject serializeAdvancement() {
+                return null;
+            }
+
+            @Override
+            public ResourceLocation getAdvancementId() {
+                return null;
+            }
+        });
+    }
+}
